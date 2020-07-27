c5ee857047ba9c65d10655739c5ee17063999d6c
AmyKhalil <acmkhy@gmail.com>
2020-07-27 17:13:32 +0200

    Switch toegevoegd en muziek geimporteerd

---
+++ KhalilAmy_BPW2_V1/Assets/Collider_Room.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider_Room : MonoBehaviour
+{
+
+    private bool IsNight; 
+    public Material DaySkybox; 
+    public Material NightSkybox; 
+    public AudioClip GreekMusic; 
+    private AudioSource muziekSpeler; 
+    private bool muziekGespeeld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        muziekSpeler = this.gameObject.GetComponent<AudioSource>();
+        RenderSettings.skybox = DaySkybox;      
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "TriggerRoomBox")
+        {
+            if(IsNight){
+                RenderSettings.skybox = DaySkybox; 
+            }
+            else{
+                RenderSettings.skybox = NightSkybox;
+            }
+            IsNight = !IsNight;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(muziekGespeeld == false){
+            if(other.gameObject.tag == "TriggerRoomBox") {
+                muziekSpeler.clip = GreekMusic;
+                muziekSpeler.Play(0);
+		        muziekGespeeld = true; 
+            }
+      }
+       
+    }
+
+}
